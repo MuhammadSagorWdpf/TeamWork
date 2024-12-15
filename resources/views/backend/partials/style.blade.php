ea0bc101263182a320cc5d1a2642d782a3eefddb
toufiqur35 <sobujts57@gmail.com.com>
2024-12-15 15:53:17 +0600

    some fixed

--- resources/views/backend/partials/style.blade.php
+++ resources/views/backend/partials/style.blade.php
@@ -7,13 +7,13 @@
     <!-- Slick CSS -->
    
    
-    <link rel="stylesheet" href="{{assets('assets/css/plugins/bootstrap.min.css')}}" />
-    <link rel="stylesheet" href="{{assets('assets/css/plugins/aos.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/bootstrap.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/aos.css')}}" />
 
     <!-- custom css -->
-    <link rel="stylesheet" href="{{assets('assets/css/style.css')}}" />
-    <link rel="stylesheet" href="{{assets('assets/css/responsive.css')}}" />
-    <link rel="stylesheet" href="{{assets('assets/css/notification.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/notification.css')}}" />
     
     <link
       rel="stylesheet"
