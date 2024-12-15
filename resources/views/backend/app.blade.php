c2d15997898b39752bb5fbb027c7cd874a9b1ac1
toufiqur35 <sobujts57@gmail.com.com>
2024-12-15 14:54:04 +0600

    asset file

---
+++ resources/views/backend/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @include('backend.partials.style')
+</head>
+<body>
+    
+    @include('backend.partials.header')
+    @yield('content')
+    @include('backend.partials.scripts')
+</body>
+</html>
