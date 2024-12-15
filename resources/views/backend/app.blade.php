587e5b583ceb0498307393fb4ca2745c88b10a63
Muhammad Sagor <sagorwdpf@gmail.com>
2024-12-15 15:28:33 +0600

    change

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @include('backend.partials.style');
+    @include('backend.partials.style')
 </head>
 <body>
     <div>
-        
+        @include('backend.partials.header')
     </div>
 </body>
 </html>
